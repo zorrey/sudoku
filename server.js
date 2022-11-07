db88f6b242ba9a7064a12d0dba98624f1e54257f
Zorrey <102656748+zorrey@users.noreply.github.com>
2022-11-07 21:45:03 +0200

    update

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ const bodyParser  = require('body-parser');
 const expect      = require('chai').expect;
 const cors        = require('cors');
 
-const fccTestingRoutes  = require('./routes/fcctesting.js');
 const apiRoutes         = require('./routes/api.js');
-const runner            = require('./test-runner');
 
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,9 +18,6 @@ app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
-
-//For FCC testing purposes
-fccTestingRoutes(app);
 
 // User routes
 apiRoutes(app);
@@ -39,18 +33,6 @@ app.use(function(req, res, next) {
 const PORT = process.env.PORT || 3000
 app.listen(PORT, function () {
   console.log("Listening on port " + PORT);
-  //process.env.NODE_ENV='test'
-  if (process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch (error) {
-        console.log('Tests are not valid:');
-        console.error(error);
-      }
-    }, 1500);
-  }
 });
 
-module.exports = app; // for testing
+
